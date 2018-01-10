7d42ffad019d4918c6eafa863d584cd3a1c97ab9
Ralf Westphal <info@ralfw.de>
2018-01-10 16:38:53 +0100

    Gerüst für ToDictionary

---
+++ Hausaufgaben/todictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace todictionary
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var d = Converter.ToDictionary("a=1");     
+            Debug.Assert(d["a"] == "1");
+        }
+    }
+
+    class Converter
+    {
+        public static IDictionary<string, string> ToDictionary(string input)
+        {
+            return null;
+        }
+    }
+}
