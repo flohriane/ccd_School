530b922744aeaa40a9cd2d0481b1e1520df33176
Ralf Westphal <info@ralfw.de>
2018-01-18 21:11:22 +0100

    kleine optimierung

--- Hausaufgaben/todictionary/Program.cs
+++ Hausaufgaben/todictionary/Program.cs
@@ -11,29 +11,19 @@ namespace todictionary
         public static Dictionary<string, string> ToDict(string configuration) 
         {
             // "a=1;b=2" -> ["a=1", "b=2"]
-            string[] zuweisungen = Program.Ich_erkenne_Semikolons(configuration);
+            string[] zuweisungen = Ich_erkenne_Semikolons(configuration);
             
             // ["a=1", "b=2"] -> [ ["a", "1"], ["b", "2"] ]
-            List<string[]> paare = Program.Ich_erkenne_Gleichheitszeichen(zuweisungen);
+            List<string[]> paare = Ich_erkenne_Gleichheitszeichen(zuweisungen);
             
             // [ ["a", "1"], ["b", "2"] ] -> { {"a","1"}, {"b","2"} }
-            Dictionary<string,string> dict = Program.Ich_baue_ein_Dictionary(paare);
+            Dictionary<string,string> dict = Ich_baue_ein_Dictionary(paare);
             return dict;
         }
-    }
-
-    internal class Program
-    {
-        public static void Main()
-        {
-            var configuration = "a=1;b=2;c=3";  // Beispiel
-
-            var d = Converter2.ToDict(configuration);
-            Assert(d["a"] == "1");
-        }
-
+        
+        
         // Operation
-        public static string[] Ich_erkenne_Semikolons(string configuration)
+        static string[] Ich_erkenne_Semikolons(string configuration)
         {
             string[] zuweisungen = configuration.Split(';');
 
@@ -41,7 +31,7 @@ namespace todictionary
         }
 
         // Operation
-        public static List<string[]> Ich_erkenne_Gleichheitszeichen(string[] zuweisungen)
+        static List<string[]> Ich_erkenne_Gleichheitszeichen(string[] zuweisungen)
         {
             List<string[]> paare = new List<string[]>();
 
@@ -54,19 +44,26 @@ namespace todictionary
         }
 
         // Operation
-        public static Dictionary<string, string> Ich_baue_ein_Dictionary(List<string[]> paare)
+        static Dictionary<string, string> Ich_baue_ein_Dictionary(List<string[]> paare)
         {
             Dictionary<string, string> DicEinzelwerte = new Dictionary<string, string>();
 
-            foreach (string[] paar in paare)
-            {
-                for (int i = 1; i < (paar.Length); i++)
-                {
-                    DicEinzelwerte.Add($"{paar[i - 1]}", $"{paar[i]}");
-                }
+            foreach (string[] paar in paare) {
+                    DicEinzelwerte.Add(paar[0], paar[1]);
             }
 
             return DicEinzelwerte;
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main()
+        {
+            var configuration = "a=1;b=2;c=3";  // Beispiel
+
+            var d = Converter2.ToDict(configuration);
+            Assert(d["a"] == "1");
         }
 
         static void Assert(bool condition)
