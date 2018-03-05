8d60259c150ef34a0b07240d66d92e516ce3ea19
Christiane Flohr <christiane.flohr@srh.de>
2018-03-05 08:52:44 +0100

    Test - Bedingung unklar

--- Fröhliche Zahlen/Fröhliche Zahlen/Fröhliche_Zahlen_testen/Class1.cs
+++ Fröhliche Zahlen/Fröhliche Zahlen/Fröhliche_Zahlen_testen/Class1.cs
@@ -13,11 +13,11 @@ namespace Fröhliche_Zahlen_testen
         [Test]
         public void Fröhliche_Zahlen_finden()
         {
-            var result = Fröhliche_Zahlen.Fröhliche_Zahlen.Zahlen_verarbeiten(10,20); // gibt Dictionary<int, bool> zurück
-            Assert.AreEqual(true, result[10]);
-            Assert.AreEqual(false, result[11]);
-            Assert.AreEqual(true, result[19]);
-            Assert.AreEqual(21, result.Count);
+            var result = Fröhliche_Zahlen.Fröhliche_Zahlen.Zahlen_verarbeiten(10,20); // gibt List<int> zurück
+            
+            // Wie prüfe ich, dass Einträge in der Liste sind ??? Bedingung ???
+            Assert.???(result[10]);
+            Assert.???(result[19]);
         }
     }
 }
