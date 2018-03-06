2a3110c46fb3cfd62370d4bbbcad6db8bbeee877
Christiane Flohr <christiane.flohr@srh.de>
2018-03-06 15:57:36 +0100

    erster Commit
    Klasse und Testklasse angelegt

---
+++ Happy_Numbers/happy_numbers/happy_numbers_testen/Happy_Numbers_Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using happy_numbers;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace happy_numbers_testen
+{
+    [TestFixture]
+    public class Happy_Numbers_Tests
+    {      
+    }
+}
