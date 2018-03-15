7808b59dd6329c07f4fc02383501f510c07bd8e5
Christiane Flohr <christiane.flohr@srh.de>
2018-03-15 16:11:23 +0100

    StreamReader ausprobiert

--- more/more/more/Program.cs
+++ more/more/more/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,20 @@ namespace more
     {
         static void Main(string[] args)
         {
+            int zähler = 0;
+            string zeile;
+
+            StreamReader pfad = new StreamReader (@"C:\Users\FlohrCh\Documents\ccd School\Weinachtsgedicht.txt");
+
+            while ((zeile = pfad.ReadLine()) != null)
+            {
+                Console.WriteLine(zeile);
+                zähler++;
+            }
+
+            pfad.Close();
+
+            Console.WriteLine("Die Datei enthält {0} Zeilen", zähler);
         }
     }
 }
