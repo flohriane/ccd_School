347a5065774e0c58a4e104daff15e82f0ad59fe8
Christiane Flohr <christiane.flohr@srh.de>
2018-03-19 11:43:55 +0100

    Entwurf

---
+++ more/more/more/More.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace more
+{
+    public class More
+    {
+        private static void More_starten()
+        {
+
+            StreamReader text = new StreamReader(@"C:\Users\FlohrCh\Documents\ccd School\Weinachtsgedicht.txt");
+
+            var zähler = Text_zeilenweise_lesen(text);
+        }
+
+
+    //Console.WriteLine("Die Datei enthält {0} Zeilen", zähler);           
+    private static int Text_zeilenweise_lesen(StreamReader text)
+    {
+        int zähler = 0;
+        string zeile;
+
+
+        while ((zeile = text.ReadLine()) != null)
+        {
+            Console.WriteLine(zeile);
+            zähler++;
+        }
+
+        text.Close();
+
+        return zähler;
+
+    }
+}
+}
---
+++ more/more/more_tests/More_Tests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace more_tests
+{
+    public class More_Tests
+    {
+
+
+    }
+}
