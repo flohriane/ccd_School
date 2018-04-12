65f109302418875ecbf0bcd696e16340b7e7067e
Christiane Flohr <christiane.flohr@srh.de>
2018-04-12 08:35:04 +0200

    initial commit

---
+++ wrapper/wrapper/Wrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wrapper
+{
+    public class Wrapper
+    {
+        public string Wrap(string text, int maxLineLength)
+        {
+            var words = Split_into_words(text);
+            return Create_lines_from_words(words, maxLineLength);
+        }
+
+        private string[] Split_into_words(string text)
+        {
+            throw new NotImplementedException();
+        }
+        private string Create_lines_from_words(string[] words, int maxLineLength)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ wrapper/wrapper_test/Testings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using wrapper;
+
+namespace wrapper_test
+{
+    [TestFixture]
+    public class Testings
+    {
+        [Test]
+        public void Wrap_words_fitting_in_line()
+        {
+            var sut = new Wrapper();
+            //Mock.NonPublic.Arrange<string[]>(sut, "Split_into_words", "zeile1 zeile2")
+            //              .Returns(new[] { "zeile1", "zeile2" }).MustBeCalled();
+            //Mock.NonPublic.Arrange<string>(sut, "Create_lines_from_words", new[] { "zeile1", "zeile2" }, 6)
+            //              .Returns("zeile1\nzeile2").MustBeCalles();    
+
+            var result = sut.Wrap("zeile1 zeile2", 6);
+
+            Assert.AreEqual("zeile1\nzeile2", result);
+            //Mock.Assert(sut);
+        }
+    }
+}
