c326df1d8c62359bb12fb3bb4442af80df3873c9
Christiane Flohr <christiane.flohr@srh.de>
2018-05-31 10:06:55 +0200

    Tests überarbeitet

--- Tic Tac Toe/tictactoe/tictactoe_tests/TicTacToe_Tests.cs
+++ Tic Tac Toe/tictactoe/tictactoe_tests/TicTacToe_Tests.cs
@@ -14,8 +14,25 @@ namespace tictactoe_tests
         [Test]
         public void Kommando_test()
         {
-            var ergebnis = TicTacToe.Kommando_ausführen(TicTacToe.Spielfeld_neu(), "a0", "X");
+            var spielfeld = new string[6, 6];
+
+            var ergebnis = TicTacToe.Kommando_ausführen(spielfeld, "neu", "X");
+            Assert.AreEqual(ergebnis[0, 1], "A");
+            Assert.AreEqual(ergebnis[1, 0], "0");
+            Assert.AreEqual(ergebnis[1, 1], " ");
+            Assert.AreEqual(ergebnis[4, 4], "+");
+            Assert.AreEqual(ergebnis[5, 4], "|");
+
+            ergebnis = TicTacToe.Kommando_ausführen(spielfeld, "a0", "X");
             Assert.AreEqual(ergebnis[1, 1], "X");
+
+            ergebnis = TicTacToe.Kommando_ausführen(spielfeld, "c2", "O");
+            Assert.AreEqual(ergebnis[5, 5], "O");
+
+            ergebnis = TicTacToe.Kommando_ausführen(spielfeld, "ende", "X");
+            Assert.AreEqual(ergebnis[5, 5], "O");
+            Assert.AreEqual(ergebnis[1, 1], "X");
+
         }
 
         [Test]
@@ -26,8 +43,15 @@ namespace tictactoe_tests
             spielfeld[1, 3] = "X";
             spielfeld[1, 5] = "X";
 
-            var ergebnis = TicTacToe.Gewinner_ermitteln(spielfeld, "X");
-            Assert.AreEqual(ergebnis, "X");
+            var gewinner = TicTacToe.Gewinner_ermitteln(spielfeld, "X");
+            Assert.AreEqual(gewinner, "X");
+
+            spielfeld[1, 5] = "O";
+            spielfeld[3, 3] = "O";
+            spielfeld[5, 1] = "O";
+
+            gewinner = TicTacToe.Gewinner_ermitteln(spielfeld, "O");
+            Assert.AreEqual(gewinner, "O");
 
             spielfeld[1, 5] = "O";
             spielfeld[3, 1] = "O";
@@ -37,12 +61,12 @@ namespace tictactoe_tests
             spielfeld[5, 3] = "O";
             spielfeld[5, 5] = "X";
 
-            ergebnis = TicTacToe.Gewinner_ermitteln(spielfeld, "X");
-            Assert.AreEqual(ergebnis, "unentschieden");
+            gewinner = TicTacToe.Gewinner_ermitteln(spielfeld, "X");
+            Assert.AreEqual(gewinner, "unentschieden");
 
             spielfeld[5, 5] = " ";
-            ergebnis = TicTacToe.Gewinner_ermitteln(spielfeld, "O");
-            Assert.AreEqual(ergebnis, "");
+            gewinner = TicTacToe.Gewinner_ermitteln(spielfeld, "O");
+            Assert.AreEqual(gewinner, "");
         }
 
         [Test]
