bf3ba08f0e2d836ded7d15b1f873e688d2bd79c8
Christiane Flohr <christiane.flohr@srh.de>
2018-06-19 13:47:23 +0200

    initial commit stack

---
+++ Stack/stack/stack/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack
+{
+    public class Stack
+    {
+        private int zaehler = 0;
+        private List<int> Einträge = new List<int>();
+
+        public void push(int eintrag)
+        {
+            Einträge.Add(eintrag);
+
+            zaehler = zaehler + 1;
+        }
+
+        public int count()
+        {
+            return zaehler;
+        }
+
+        public int pop()
+        {
+            int letzterEintrag = Einträge.Last();
+
+            Einträge.RemoveAt(Einträge.Count-1);
+
+            return letzterEintrag;
+        }
+    }
+}
---
+++ Stack/stack/stack/Stack_Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace stack
+{
+    [TestFixture]
+    class Stack_Tests
+    {
+        [Test]
+        public void PushUndCount()
+        {
+            var stack = new Stack();
+            stack.push(10);
+            var result = stack.count();
+            Assert.AreEqual(1, result);
+        }
+        [Test]
+        public void PushUndPop()
+        {
+            var stack = new Stack();
+            stack.push(20);
+            var result = stack.pop();
+            Assert.AreEqual(20,result);
+        }
+
+        [Test]
+        public void PushTwoEntries()
+        {
+            var stack = new Stack();
+            stack.push(10);
+            stack.push(15);
+            var result = stack.count();
+            Assert.AreEqual(2,result);
+        }
+        [Test]
+        public void PopTwoEntries()
+        {
+            var stack = new Stack();
+            stack.push(20);
+            stack.push(30);
+            Assert.AreEqual(30, stack.pop());
+            Assert.AreEqual(20, stack.pop());
+        }
+    }
+}
